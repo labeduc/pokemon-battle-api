a111b718a1b68e3c3bde1b14fcc705a7496d88f9
WalterPaixaoCortes <wpcortes@gmail.com>
2024-10-31 23:32:23 -0300

    fix: removing o reload

--- main.py
+++ main.py
@@ -192,4 +192,4 @@ async def run_battle(batalha: Batalha) -> Any:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
